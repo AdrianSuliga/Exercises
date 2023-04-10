b7bbb993788aa2c2b2af9a5f534dd001e34bdac2
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-04-10 13:42:13 +0200

    Final answer

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 string dane_to_bin(string dane);
 string bin_to_dane(string dane);
+bool correct_date(int year, int month, int day);
 
 int main()
 {
@@ -30,74 +31,77 @@ int main()
 string dane_to_bin(string dane)
 {
 	string answer = "", year = "", month = "", day = "";
-	int fst = 0, snd = 0, licznik = 0, y = 0, m = 0, d = 0;
+	int licznik = 0, y = 0, m = 0, d = 0;
+	bool flag = true;
 
-	for (int i = 0; i < dane.length(); i++)
-	{
-		if (dane[i] == '-')
-		{
-			licznik++;
-			if (licznik == 1)
-				fst = i;
-			if (licznik == 2)
-				snd = i;
-		}
-	}
-	for (int i = 0; i < fst; i++)
+	if (dane.length() != 10)
+		return "ERROR";
+
+	for (int i = 0; i < 4; i++)
 		year += dane[i];
-	for (int i = fst + 1; i < snd; i++)
+	for (int i = 5; i < 7; i++)
 		month += dane[i];
-	for (int i = snd + 1; i < dane.length(); i++)
+	for (int i = 8; i < 10; i++)
 		day += dane[i];
 
 	y = atoi(year.c_str());
 	m = atoi(month.c_str());
 	d = atoi(day.c_str());
 
-	y -= 1980;
-	
-	licznik = 64;
-	year = "0000000";
-	for (int i = 0; i < 7; i++)
+	flag = correct_date(y, m, d);
+	if (flag == true)
 	{
-		if (y / licznik != 0)
-		{
-			year[i] = '1';
-			y -= licznik;
-		}
-		licznik /= 2;
-	}
-	licznik = 8;
-	month = "0000";
-	for (int i = 0; i < 4; i++)
-	{
-		if (m / licznik != 0)
-		{
-			month[i] = '1';
-			m -= licznik;
-		}
-		licznik /= 2;
-	}
+		y -= 1980;
 
-	licznik = 16;
-	day = "00000";
-	for (int i = 0; i < 5; i++)
-	{
-		if (d / licznik != 0)
+		licznik = 64;
+		year = "0000000";
+		for (int i = 0; i < 7; i++)
 		{
-			day[i] = '1';
-			d -= licznik;
+			if (y / licznik != 0)
+			{
+				year[i] = '1';
+				y -= licznik;
+			}
+			licznik /= 2;
 		}
-		licznik /= 2;
-	}
+		licznik = 8;
+		month = "0000";
+		for (int i = 0; i < 4; i++)
+		{
+			if (m / licznik != 0)
+			{
+				month[i] = '1';
+				m -= licznik;
+			}
+			licznik /= 2;
+		}
 
-	answer += year + month + day;
-	return answer;
+		licznik = 16;
+		day = "00000";
+		for (int i = 0; i < 5; i++)
+		{
+			if (d / licznik != 0)
+			{
+				day[i] = '1';
+				d -= licznik;
+			}
+			licznik /= 2;
+		}
+
+		answer += year + month + day;
+		return answer;
+	}
+	if (flag == false)
+		return "ERROR";
 }
 string bin_to_dane(string dane)
 {
 	string answer = "", year = "", month = "", day = "";
 	int y = 0, m = 0, d = 0, licznik = 0;
+	bool flag = true;
+
+	if (dane.length() != 16)
+		return "ERROR";
 
 	for (int i = 0; i < 7; i++)
 		year += dane[i];
@@ -130,26 +134,57 @@ string bin_to_dane(string dane)
 			d += licznik;
 		licznik *= 2;
 	}
+	flag = correct_date(y, m, d);
+	if (flag == true)
+	{
+		year = "";
+		month = "";
+		day = "";
 
-	year = "";
-	month = "";
-	day = "";
+		string finalm = "", finald = "";
 
-	string finalm = "", finald = "";
+		year += to_string(y);
+		month += to_string(m);
+		day += to_string(d);
 
-	year += to_string(y);
-	month += to_string(m);
-	day += to_string(d);
+		if (month.length() < 2)
+			finalm = finalm + "-" + "0" + month;
+		else
+			finalm = finalm + "-" + month;
+		if (day.length() < 2)
+			finald = finald + "-" + "0" + day;
+		else
+			finald = finald + "-" + day;
 
-	if (month.length() < 2)
-		finalm = finalm + "-" + "0" + month;
-	else
-		finalm = finalm + "-" + month;
-	if (day.length() < 2)
-		finald = finald + "-" + "0" + day;
-	else
-		finald = finald + "-" + day;
+		answer += year + finalm + finald;
+		return answer;
+	}
+	if (flag == false)
+		return "ERROR";
+}
+bool correct_date(int year, int month, int day)
+{
+	cout << year << " " << month << " " << day << endl;
 
-	answer += year + finalm + finald;
-	return answer;
+	if (year < 1980 || year > 2107)
+		return false;
+
+	if (month < 1 || month > 12)
+		return false;
+
+	if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && 
+		(day < 1 || day > 31))
+		return false;
+
+	if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30))
+		return false;
+
+	if (month == 2)
+	{
+		if (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) && (day < 1 || day > 29))
+			return false;
+		else if (day < 1 || day > 28)
+			return false;
+	}
+	return true;
 }
